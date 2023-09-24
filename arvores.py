b47988a699e8b00db431d9368f8150577a95656f
bramau9010 <78278384+bramau9010@users.noreply.github.com>
2023-09-24 16:03:18 -0300

    Update arvores.py

--- arvores.py
+++ arvores.py
@@ -1,188 +1,78 @@
-import matplotlib.pyplot as plt
-import random
+import tkinter as tk
+from tkinter import ttk
 
 
-class Node:
-    """
-    Class Node
-    """
-    def __init__(self, value):
-        self.left = None
-        self.data = value
-        self.right = None
+from arvores import Tree
+from arvores import Node
 
-class Tree:
-    """
-    Class tree will provide a tree as well as utility functions.
-    """
+VALID_ARVORES = [
+    "Pequeno", "Medio", "Grande"
+]
 
-    def createNode(self, data):
-        """
-        Utility function to create a node.
-        """
-        return Node(data)
+class ArvoreWidget(ttk.Widget):
+    def __init__(self, master, **kw):
+        super().__init__(master, "ttk::frame", kw)
 
-    def insert(self, node , data):
-        """
-        Insert function will insert a node into tree.
-        Duplicate keys are not allowed.
-        """
-        #if tree is empty , return a root node
-        if node is None:
-            return self.createNode(data)
-        # if data is smaller than parent , insert it into left side
-        if data < node.data:
-            node.left = self.insert(node.left, data)
-        elif data > node.data:
-            node.right = self.insert(node.right, data)
+        self.from_label =  ttk.Label(self, text="Criar uma árvore:")
+        self.from_label.pack()
+        self.from_box = ttk.Combobox(self, values=VALID_ARVORES)
+        self.from_box.pack()
 
-        return node
 
-    def traverseInorder(self, root):
-        """
-        traverse function will print all the node in the tree.
-        """
-        if root is not None:
-            self.traverseInorder(root.left)
-            print (root.data)
-            self.traverseInorder(root.right)
-
-    def traversePreorder(self, root):
-        """
-        traverse function will print all the node in the tree.
-        """
-        if root is not None:
-            print (root.data)
-            self.traversePreorder(root.left)
-            self.traversePreorder(root.right)
-
-    def traversePostorder(self, root):
-        """
-        traverse function will print all the node in the tree.
-        """
-        if root is not None:
-            self.traversePostorder(root.left)
-            self.traversePostorder(root.right)
-            print (root.data)
-
-    
-
-           
-    def inverse(self, root):
-        if root is not None:
-            # Swap the left and right subtrees
-            root.left, root.right = root.right, root.left
-            
-            # Recursively invert the left and right subtrees
-            self.inverse(root.left)
-            self.inverse(root.right)
-            
-        return root
-    
-    def inverse_subtree(self, root, target_depth, current_depth=1):
-        if root is not None:
-            if current_depth == target_depth:
-                # Swap the left and right subtrees at the target depth
-                root.left, root.right = root.right, root.left
-            else:
-                # Recursively invert the left and right subtrees at deeper levels
-                self.inverse_subtree(root.left, target_depth, current_depth + 1)
-                self.inverse_subtree(root.right, target_depth, current_depth + 1)
-        return root
-    
-    def navigate_and_inverse(self, root, position_sequence):
-        node = root
+    def get(self):
+        # pega o valor do Entry
         
-        # Iterate through the position sequence
-        for direction in position_sequence:
-            if direction == 0 and node.left is not None:
-                node = node.left
-            elif direction == 1 and node.right is not None:
-                node = node.right
+        tamanho = self.from_box.get()
+
+        if tamanho == 'Pequeno':
+            tree= Tree()
+            tree,root = tree.random_tree(4)           
+
+            return tree,root
+        elif tamanho == 'Medio':
+            tree= Tree()
+            tree,root = tree.random_tree(8)           
+
+            return tree,root
+        elif tamanho == 'Grande':
+            tree= Tree()
+            tree,root = tree.random_tree(12)           
+
+            return tree,root
+        else:
+            pass
         
-        # Invert the subtree rooted at the selected node
-        self.inverse(node)  # Assuming you want to start inversion from the selected node down
-        
-        return root
-
-
-
-
-
-            
-    def _plot_node(self, ax, node, x=0, y=0, dx=1, dy=1):
-        if node.left:
-            ax.plot([x, x - dx], [y, y - dy], 'k-')
-            ax.text(x - dx*0.1, y, str(node.data), ha='center',
-                    va='center', bbox=dict(facecolor='white', edgecolor='black'), rotation=25)
-
-            self._plot_node(ax, node.left, x - dx, y - dy, dx*0.5, dy)
-        if node.right:
-            ax.plot([x, x + dx], [y, y - dy], 'k-')
-            ax.text(x + dx*0.1, y, str(node.data), ha='center',
-                    va='center', bbox=dict(facecolor='white', edgecolor='black'), rotation=25)
-            self._plot_node(ax, node.right, x + dx, y - dy, dx*0.5, dy)
-
-        if node.left is None and node.right is None:
-            ax.text(x, y, str(node.data), ha='center', va='center',
-                    bbox=dict(facecolor='white', edgecolor='black'))
-
-            
-    def plot_tree(self, root):
-        fig, ax = plt.subplots(figsize=(8, 6))
-        self._plot_node(ax, root)
-        plt.axis('off')
-        plt.show()
-
-        
-    '''Cria uma árvore de maneira aleatoria, vai inseriendo um por vez'''
-
-
-
-    def random_tree(self, size=42):
-        values = random.sample(range(1, 1000), size)
-        tree = Tree()
-        root = None  # Initialize the root
-
-        for v in values:
-            root = tree.insert(root, v)  # Create and insert a Node with the value v
-
-        return tree,root
-
-
-
-
-            
-
 
 def main():
-    # root = None
-    tree = Tree()
-    # root = tree.insert(root, int(input("coloque a raiz: ")))
-    # root = tree.insert(root, int(input("coloque un nodo: ")))
-    # root = tree.insert(root, int(input("coloque un nodo: ")))
-    # root = tree.insert(root, int(input("coloque un nodo: ")))
-    # root = tree.insert(root, int(input("coloque un nodo: ")))
-    # root = tree.insert(root, int(input("coloque un nodo: ")))
-    # root = tree.insert(root, int(input("coloque un nodo: ")))
 
-    # print ("\n Traverse Inorder")
-    # tree.traverseInorder(root)
-    # print ("\n Traverse Preorder")
-    # tree.traversePreorder(root)
-    # print ("\n Traverse Posorder")
-    # tree.traversePostorder(root)
+    def plotar():
+        # TODO: fazer voltar a funcionar
+        tree,root = arvore_widget.get()
+        arvore = tree.plot_tree(root)
+        # Draw the plot on the canvas
+        canvas.delete("all")  # Clear previous drawings
+        canvas.create_image(0, 0, anchor=tk.NW, image=arvore)
+        
 
+    window = tk.Tk()
 
-    # tree.plot_tree(root)
-    # tree.plot_tree(tree.navigate_and_inverse(root,[0]))
+    mainframe = ttk.Frame(window, padding=32)
+    mainframe.pack()
 
-    tree,root = tree.random_tree(12)
-    tree.plot_tree(root)
+    arvore_widget = ArvoreWidget(mainframe)
+    arvore_widget.pack()
+
+    button = ttk.Button(mainframe, text="Plotar", command=plotar)
+    button.pack()
+
+    # Create a Canvas widget to display the plot
+    canvas = tk.Canvas(mainframe, width=400, height=400)
+    canvas.pack()
 
     
-    
-   
 
-if __name__ == "__main__":
+    window.mainloop()
+    print("Acabou")
+
+if __name__ == '__main__':
     main()
